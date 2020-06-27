a3609873784479e3e6bd157eec12a142b04249cc
venaaa <sergebrew@gmail.com>
2020-06-27 03:03:26 +0300

    Added <models>, <forms> stored it in db and edited html template

--- landing/views.py
+++ landing/views.py
@@ -1,9 +1,17 @@
 from django.shortcuts import render
 from datetime import datetime
+from .forms import SubscriberForm
 
 # Create your views here.
 def landing(request):
 
     now = datetime.now()
     name = 'Serge'
+    form = SubscriberForm(request.POST or None)
+    if request.method == 'POST' and form.is_valid():
+        data = form.cleaned_data
+        print(data['name'], data['email'])
+
+        saved_form = form.save()
+
     return render(request, 'landing/index.html', locals())
